f3df284edc5fed90e6563d3725ff1a0ed9ca96c1
Dominik Rein <d.rein@science-computing.de>
2019-11-06 11:33:39 +0100

    Switch to php

--- Client_Website/artikel.php
+++ Client_Website/artikel.php
@@ -5,6 +5,9 @@
         <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
+        <?php
+            $tischnummer = $_POST["inputTischnr"]
+        ?>
         <h1>Homepage Headline</h1>
         <p>This is a paragraph.</p>
     </body>
