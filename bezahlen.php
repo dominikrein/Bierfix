30abf6b14f8474e20d7378716e78ef209ab7919c
Dominik Rein <d.rein@science-computing.de>
2019-11-08 13:22:40 +0100

    post an drucker todo

--- bezahlen.php
+++ bezahlen.php
@@ -88,7 +88,7 @@
             function abgerechnet(){
                 if(gesamtabrechnen){
                     bestellungAbsenden();  
-                    //window.open("index.php","_self");                  
+                    window.open("index.php","_self");                  
                 }
                 else{
                     //Es wurde nur eine Auswahl abgerechnet
@@ -138,25 +138,24 @@
                 var bst = JSON.parse(sessionStorage.getItem("kompletteBestellung"));
 
                 //create XML to transfer to php
-                var xml_begin = `<info bediener="${bediener}" tischnummer="${tischnummer}" /><artikel>`;
+                var xml_header = "\<?xml version='1.0' standalone='yes'?>";
+                var xml_begin = `<bestellung bediener="${bediener}" tischnummer="${tischnummer}">`;
                 var xml_content = "";
                 for(var i = 0; i < bst.length; i++){
                     //      0           1          2    3       4       5       6
                     // artikelId, bezeichnung, menge, preis, anzahl, auswahl, typ
                     xml_content = xml_content + `<artikel id="${bst[i][0]}" bezeichnung="${bst[i][1]}" menge="${bst[i][2]}" preis="${bst[i][3]}" anzahl="${bst[i][4]}" typ="${bst[i][6]}" />`;
                 }
-                var xml_end = "</artikel";
-                var xml = xml_begin + xml_content + xml_end;
-
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'drucken.php', true);
-                xhr.setRequestHeader('Content-type', 'text/xml; charset=utf-8');
-                xhr.onload = function () {
-                    // do something to response
-                    console.log(this.responseText);
-                };
-                console.log(xml);
-                xhr.send(xml);
+                var xml_end = "</bestellung>";
+                var xml = xml_header + xml_begin + xml_content + xml_end;
+                $.ajax({
+                    url: 'drucken.php',
+                    type: 'POST',
+                    data: {xml:xml},
+                    success: function(data){
+                        console.log(data);
+                    }
+                });
             }
 
             window.onload = function(){
--- drucken.php
+++ drucken.php
@@ -1,38 +1,88 @@
 <?php
-    
-    (array)$bestellung = json_decode($_POST['postBestellung']);
-    $tischnummer = $_POST['postTischnummer'];
-    $bediener = $_POST['postBediener'];
-    $datum = date("%a %d.%m.%j  %H:%M");
-    $url = 'http://192.168.2.172/cgi-bin/epos/service.cgi?devid=epson&timeout=10000';
-    $content = "";
-    file_put_contents("file.txt", $_POST['postBestellung']);
-foreach($bestellung as $artikel){
-    $anzahl = $artikel[4];
-    $beschreibung = $artikel[1] . $artikel[2];
-    $preis = $artikel[3] * $anzahl;
-    $content += "<feed/> <text smooth=\"true\" align=\"left\" reverse=\"false\">$anzahl x $beschreibung  $preis €</text>";
-}
+    //EINSTELLUNGEN
+        $festTitel = center("2. Öschinger Weideabtrieb");
+        $printerIp = "192.168.2.172";
 
-$request_begin = <<<EOD
+        $artikeltypen = array(
+            '1'=>'Getränke',
+            '2'=>'Speisen',
+        );
+
+
+    //ENDE EINSTELLUNGEN
+
+    function center($text){
+        //Eine ungerade Zahl nötiger Leerzeichen kann vermutlich vernachlässigt werden.
+        $notwendigeLeerzeichen = 42 - strlen($text);
+        $leerzeichen = "";
+        for($i = 0; $i < ($notwendigeLeerzeichen / 2); $i++){
+            $leerzeichen = $leerzeichen . " ";
+        }
+        return $leerzeichen . $text;             
+    }
+
+    /*  <?xml version='1.0' standalone='yes'?>
+        <bestellung bediener="bla" tischnummer="50">
+            <artikel id="0" bezeichnung="Bier" menge="0.5l" preis="3" anzahl="1" typ="1" />
+            <artikel id="1" bezeichnung="Radler" menge="0.5l" preis="3" anzahl="1" typ="1" />
+            <artikel id="9" bezeichnung="Pommes" menge="Schale" preis="2.5" anzahl="1" typ="2" />
+            <artikel id="10" bezeichnung="Burger" menge="Stueck" preis="2.5" anzahl="1" typ="2" />
+        </bestellung>
+    */
+         
+    $bestellung = new SimpleXMLElement($_POST["xml"]);
+    $bediener = $bestellung['bediener'];
+    $tischnummer = $bestellung['tischnummer'];
+    $url = 'http://' . $printerIp . '/cgi-bin/epos/service.cgi?devid=epson&timeout=10000';
+    $settings = new SimpleXMLElement(file_get_contents("settings.xml"));
+    
+    $content = "";
+    $aktuellerTyp = "";
+
+    foreach($bestellung->artikel as $artikel){
+        $anzahl = $artikel['anzahl'];
+        $bezeichnung = $artikel['bezeichnung'];
+        $menge = $artikel['menge'];
+        $typ = $artikel['typ'];
+        $preis = $artikel['preis'] * $anzahl;
+
+        //Die Liste ist nach Typ (Essen, Getränke, ...) sortiert (irgendwo bei artikel.php).
+        //Der Typ ist int - lookup Datei: settings.xml
+        if($aktuellerTyp != $typ){
+            //Anderer Typ - Typ drucken
+            $aktuellerTyp = $typ;
+            $centered = center($artikeltypen[$typ]);
+            $content += "<feed/><text smooth="true" width="1" height="1" align="left" reverse="false">------------------------------------------</text><feed/><text smooth="true" width="1" height="1" align="left" reverse="false">$centered</text><feed/><text smooth="true" width="1" height="1" align="left" reverse="false">------------------------------------------</text>";
+        }      
+
+        //Es stehen beim Drucker exakt 42 Zeichen pro Zeile zur Verfügung 
+        //Da Preis rechtsbündig müssen die Leerzeichen berechnet werden!
+        //2x Bier 0,5l    6.50€
+        $textlaenge= strlen($anzahl) + strlen("x ") + strlen($bezeichnung) + strlen(" ") + strlen($menge) + strlen($preis) + strlen("€");
+        $leerzeichen = "";
+        for($i = 0; $i < (42 - $textlaenge); $i++){
+            $leerzeichen = $leerzeichen . " ";
+        }
+
+        $content += "<feed/> <text smooth=\"true\" align=\"left\" reverse=\"false\">" . $anzahl . "x " . $bezeichnung . " " . $menge . $leerzeichen . $preis . "€</text>";
+    }
+
+    $datum = center(date("%a %d.%m.%j  %H:%M") . " Uhr");
+    $tischnummer = center("Tischnummer: " . $tischnummer);
+
+$request = <<<EOD
 <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
 <s:Body>
 <epos-print xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print">
 <page>
 <feed/>
-<text smooth="true" align="center" width="1" height="2">         2. Öschinger Weideabtrieb&#10;</text>
+<text smooth="true" align="center" width="1" height="2">$festTitel</text>
 <feed/>
-<text smooth="true" width="1" height="1" align="left" reverse="false">            $datum Uhr</text>
-<feed/>
-<text smooth="true" width="1" height="1" align="left" reverse="false">------------------------------------------</text>
-<feed/>
-<text smooth="true"  align="center" width="2" height="2" reverse="true">  Tischnummer:  $tischnummer  </text>
-<feed/>                   
-<text smooth="true" width="1" height="1" align="left" reverse="false">------------------------------------------</text>
-<feed/>
-<text smooth="true" width="1" height="1" align="left" reverse="false">|                Artikel                 |</text>
+<text smooth="true" width="1" height="1" align="left" reverse="false">$datum</text>
 <feed/>
 <text smooth="true" width="1" height="1" align="left" reverse="false">------------------------------------------</text>
+<feed/>
+<text smooth="true"  align="center" width="2" height="2" reverse="true">$tischnummer</text>
 $content
 </page>
 <cut/>
@@ -42,61 +92,30 @@ $content
 EOD;
 
 
-$opts = array('http' =>
-    array(
-        'method'  => 'POST',
-        'header'  => ["Content-type: text/xml; charset=utf-8", "If-Modified-Since: Thu, 01 Jan 1970 00:00:00 GMT", "SOAPAction: ''"],
-        'content' => $content
-    )
-);
-# Create the context
-$context = stream_context_create($opts);
-# Get the response (you can use this for GET)
-$result = file_get_contents($url, false, $context);
+/* JavaScript POST: https://stackoverflow.com/questions/58766486/create-xmlhttprequest-from-php
+
+        //Send print document
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'text/xml; charset=utf-8');
+        xhr.setRequestHeader('If-Modified-Since', 'Thu, 01 Jan 1970 00:00:00 GMT');
+        xhr.setRequestHeader('SOAPAction', '""');
+        xhr.onreadystatechange = function () {
+
+            // Receive response document
+            if (xhr.readyState == 4) {
+
+                // Parse response document
+                if (xhr.status == 200) {
+                    alert(xhr.responseXML.getElementsByTagName('response')[0].getAttribute('success'));
+                }
+                else {
+                    alert('Network error occured.');
+                }
+            }
+        };
+
+        xhr.send(req);
+*/
 
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        
-        <script type="text/javascript">
-            // URL of ePOS-Print supported TM printer
-           
-            function button1_Click() {
-               
-
-                // Send print document
-                var xhr = new XMLHttpRequest();
-                xhr.open('POST', url, true);
-                xhr.setRequestHeader('Content-Type', 'text/xml; charset=utf-8');
-                xhr.setRequestHeader('If-Modified-Since', 'Thu, 01 Jan 1970 00:00:00 GMT');
-                xhr.setRequestHeader('SOAPAction', '""');
-                xhr.onreadystatechange = function () {
-
-                    // Receive response document
-                    if (xhr.readyState == 4) {
-
-                        // Parse response document
-                        if (xhr.status == 200) {
-                            alert(xhr.responseXML.getElementsByTagName('response')[0].getAttribute('success'));
-                        }
-                        else {
-                            alert('Network error occured.');
-                        }
-                    }
-                };
-
-                xhr.send(req);
-            }
-
-        </script>
-    </head>
-
-    <body>
-        <div style="text-align: center;">
-            <button onclick="button1_Click()">Print</button>
-        </div>
-    </body>
-</html>
